16fcafc1c2f399f47cb0b6538bf983face35caf0
Chris <chris@halcyondaze.net>
2022-09-06 13:59:05 -0700

    init

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import InputField from './components/inputfield';
+
+const App: React.FC = () => {
+	return (
+		<div className="App">
+			<span className="heading">Taskify</span>
+			<InputField />
+		</div>
+	);
+};
+
+export default App;
---
+++ src/components/inputfield.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const InputField = () => {
+	return <div></div>;
+};
+
+export default InputField;
